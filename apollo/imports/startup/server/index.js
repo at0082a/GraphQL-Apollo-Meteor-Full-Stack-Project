fd1cd47d41e1174aabbed7b2c96e624bb273873b
Alex Tilatti <alex.tilatti@gmail.com>
2019-10-03 21:36:13 -0400

    created preliminary graphql schema

--- apollo/imports/startup/server/index.js
+++ apollo/imports/startup/server/index.js
@@ -1,4 +1,23 @@
 import { createApolloServer } from "meteor/apollo";
 import { makeExecutableSchema } from "graphql-tools";
 
-createApolloServer({});
+const typeDefs = `
+type Query {
+  hi: String
+}`;
+
+//resolvers are functions that set inputs or values in the schema (TypeDefs)
+const resolvers = {
+  Query: { 
+  hi() {
+    return "hello graphql world";
+  }
+ }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({schema});
